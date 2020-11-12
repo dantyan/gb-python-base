78486ec6932c0fa7e3677add05a8938cc3f6159d
Dan <dan.tyan@gmail.com>
2020-11-12 11:55:17 +0600

    first lesson home work

--- task-1.py
+++ task-1.py
@@ -1,4 +1,5 @@
 from datetime import date
+
 today = date.today()
 
 _str = input('Введите произвольную строку: ')
--- task-3.py
+++ task-3.py
@@ -4,9 +4,7 @@ if not number.isdigit():
     print('Only numbers allowed')
     exit()
 
-n_1, n_2, n_3 = int(number), int(number*2), int(number*3)
+n_1, n_2, n_3 = int(number), int(number * 2), int(number * 3)
 
 answer = n_1 + n_2 + n_3
 print(f'Answer is: {answer}')
-
-
